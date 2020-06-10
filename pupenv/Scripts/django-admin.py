e8e07197b4a25d5e2d0b04cacc086f6f25215e80
wendell1101 <wendellchansuazo11@gmail.com>
2020-06-10 18:31:23 +0800

    initial commit

--- pupenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\pc hunter\Desktop\Learning_Management\pupenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
