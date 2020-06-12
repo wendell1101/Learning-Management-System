45bfd2684e58baba346c761c441e34257f300899
wendell1101 <wendellchansuazo11@gmail.com>
2020-06-12 14:15:16 +0800

    set border to none

--- PUP_LMS/settings.py
+++ PUP_LMS/settings.py
@@ -34,8 +34,7 @@ ALLOWED_HOSTS = ['future-academy.herokuapp.com',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
-   
+    'django.contrib.auth',   
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
